6e7d7e1e868d28f4bc8b41b9c8f60c30c15c07db
MyUsername <figusgiacomo04@gmail.com>
2024-03-01 13:30:38 +0100

    FIXED: improved code function

--- partials/functions/function.php
+++ partials/functions/function.php
@@ -5,53 +5,26 @@
         $numeri = '0123456789';
         $simboli = '!@#$%^&*()-_';
     
-        // SHUFFLE SINGOLI
-        $caratteriMescolati = str_shuffle($caratteri);
-        $numeriMescolati = str_shuffle($numeri);
-        $simboliMescolati = str_shuffle($simboli);
-    
-        // SHUFFLE COMPOSTI DA 2
-        $caratterinumeriMescolati = str_shuffle($caratteri . $numeri);
-        $caratteriEspecialiMescolati = str_shuffle($caratteri . $simboli);
-        $numerisimboliMescolati = str_shuffle($numeri . $simboli);
-    
-        // SHUFFLE DA 3
-        $AllcaratteriMescolati = str_shuffle($numeri . $simboli . $caratteriMescolati);
+        $caratteriDisponibili = '';
         
-        $lunghezza = $_GET["lunghezza"];
-
-        // CHECK OPZIONI SELEZIONATE
-        $lettere_selezionate = isset($_GET["lettere"]);
-        $numeri_selezionati = isset($_GET["numeri"]);
-        $simboli_selezionati = isset($_GET["simboli"]);
-
+        if(isset($_GET["lettere"])){
+            $caratteriDisponibili .= $caratteri;
+        }
+        if(isset($_GET["numeri"])){
+            $caratteriDisponibili .= $numeri;
+        }
+        if(isset($_GET["simboli"])){
+            $caratteriDisponibili .= $simboli;
+        }
+    
+        $caratteriMescolati = str_shuffle($caratteriDisponibili);
+        $lunghezzaCaratteri = strlen($caratteriDisponibili);
         $passwordGenerata = '';
     
-        for($i = 0; $i < $lunghezza; $i++ ){
-            
-            if($numeri_selezionati && $simboli_selezionati && $lettere_selezionate){
-                // Tutte le opzioni
-                $passwordGenerata .= $AllcaratteriMescolati[$i];
-            }elseif($numeri_selezionati && $simboli_selezionati){
-                // Numeri e simboli
-                $passwordGenerata .= $numerisimboliMescolati[$i];
-            } elseif($lettere_selezionate && $simboli_selezionati){
-                // Lettere e simboli
-                $passwordGenerata .= $caratteriEspecialiMescolati[$i];
-            } elseif($lettere_selezionate && $numeri_selezionati){
-                // Lettere e numeri
-                $passwordGenerata .= $caratterinumeriMescolati[$i];
-            } elseif($lettere_selezionate){
-                // Lettere 
-                $passwordGenerata .= $caratteriMescolati[$i];
-            } elseif($numeri_selezionati){
-                // Numeri
-                $passwordGenerata .= $numeriMescolati[$i];
-            } elseif($simboli_selezionati){
-                // Simboli
-                $passwordGenerata .= $simboliMescolati[$i];
-            }
-
-        }    
+        for($i = 0; $i < $lunghezza; $i++){
+            $carattereCasuale = $caratteriMescolati[rand(0, $lunghezzaCaratteri - 1)];
+            $passwordGenerata .= $carattereCasuale;
+        }
+    
         return $passwordGenerata;
     }   
